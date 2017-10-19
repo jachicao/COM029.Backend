47d3ab7af128e8fdefac9a801ec2128110818d9d
jachicao <jachicao@uc.cl>
2017-10-18 23:09:51 -0300

    set azure db after local config

--- src/app.js
+++ src/app.js
@@ -24,6 +24,29 @@ const app = feathers();
 
 // Load app configuration
 app.configure(configuration());
+
+if (app.get('env') === 'production') {
+  const getValue = (str, key) => {
+    const split = str.split(';');
+    for (var i = 0; i < split.length; i++) {
+      const s = split[i];
+      if (s.indexOf(key) > -1) {
+        return s.replace(key + '=', '');
+      }
+    }
+    return '';
+  };
+  const dbProperties = process.env.MYSQLCONNSTR_localdb;
+  const host = getValue(dbProperties, 'Data Source');
+  const splitHost = host.split(':');
+  app.set('db.name', getValue(dbProperties, 'Database'));
+  app.set('db.username', getValue(dbProperties, 'User Id'));
+  app.set('db.password', getValue(dbProperties, 'Password'));
+  app.set('db.host', splitHost[0]);
+  app.set('db.port', splitHost[1]);
+  app.set('db.type', 'mysql');
+}
+
 // Enable CORS, security, compression, favicon and body parsing
 app.use(cors());
 app.use(helmet());
--- src/index.js
+++ src/index.js
@@ -2,29 +2,6 @@
 const logger = require('winston');
 const app = require('./app');
 const port = app.get('port');
-
-if (app.get('env') === 'production') {
-  const getValue = (str, key) => {
-    const split = str.split(';');
-    for (var i = 0; i < split.length; i++) {
-      const s = split[i];
-      if (s.indexOf(key) > -1) {
-        return s.replace(key + '=', '');
-      }
-    }
-    return '';
-  };
-  const dbProperties = process.env.MYSQLCONNSTR_localdb;
-  const host = getValue(dbProperties, 'Data Source');
-  const splitHost = host.split(':');
-  app.set('db.name', getValue(dbProperties, 'Database'));
-  app.set('db.username', getValue(dbProperties, 'User Id'));
-  app.set('db.password', getValue(dbProperties, 'Password'));
-  app.set('db.host', splitHost[0]);
-  app.set('db.port', splitHost[1]);
-  app.set('db.type', 'mysql');
-}
-
 const server = app.listen(port);
 
 process.on('unhandledRejection', (reason, p) =>
